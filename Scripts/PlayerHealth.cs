c77a0e1798037aabe6798284ffd034ee5d561e03
Dilmiiiii <it22279798@my.sliit.lk>
2026-02-13 18:26:04 +0530

    Add files via upload

---
+++ Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died");
+        Destroy(gameObject);
+    }
+}
